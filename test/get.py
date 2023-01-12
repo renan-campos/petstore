5b1bc40c53a11484222e78ca17b91dccb0127074
Renan Campos <rcampos@redhat.com>
2023-01-11 22:38:07 -0500

    Added some more tests. Took a while.
    These tests are simple, but they capture the simple behavior that the
    backend server exhibits so far:
    - 404 is returned when the call tries to get a pet that doesn't exist
    - When a pet is created, 200 is returned and the body has the pet data
    - When the pet does exist, 200 is return and the body has the pet data
    
    The test script expects that a fresh instance of the backend is running.
    Trying to run the test a second time without a clean instance of the
    backend will fail tests.
    
    Later I'll look into making the test start up the webserver. I could do
    it all in containers. That would be pretty slick.
    
    But I need to add some new functionality to this backend. This is
    starting to become one of those projects that I don't actually work on.
    Probably because the spec has no real bearing on anything I'm actually
    doing.
    
    This is an exercise of commitment.

--- test/get.py
+++ test/get.py
@@ -9,32 +9,66 @@ petstore_url = "http://localhost:8080"
 pet_endpoint = "/api/v3/pet"
 
 def create_payload(name):
-    return {'name': name}
+    return json.dumps({'name': name})
 
 def test(description, func):
     error = func()
     if error != "":
-        return description + "failed: " + error
+        return f"{description} failed: {error}"
     return ""
 
-def create_test():
-    payload = create_payload('Brady')
-    response = requests.post(
-            url=petstore_url+pet_endpoint,
-            data=json.dumps(
-                create_payload('Brady')
+def test_create():
+    pet_name = 'Brady'
+    payload = create_payload(pet_name)
+    try:
+        response = requests.post(
+                url=petstore_url+pet_endpoint,
+                data=create_payload(pet_name)
                 )
-            )
+    except:
+        return "Failed to connect to backend"
     if (response.status_code != 200):
         return "Expected 200 status code. Got: " + str(response.status_code)
     created_pet = response.json()
-    if (created_pet['name'] != payload['name']):
+    if (created_pet['name'] != pet_name):
         return "Expected pet name: " + payload['name'] + "got: " + created_pet['name']
     return ""
 
+def test_get():
+    try:
+        response = requests.get(
+                url=f"{petstore_url}/{pet_endpoint}/0"
+                )
+    except:
+        return "Failed to connect to backend"
+    if (response.status_code != 200 ):
+        return f"Expected 200 status code. Got: {response.status_code}"
+    created_pet = response.json()
+    if (created_pet['name'] != 'Brady'):
+        return f"Expected pet name: 'Brady' got: {created_pet['name']}"
+    return ""
+
+def test_noget():
+    try:
+        response = requests.get(
+                url=f"{petstore_url}/{pet_endpoint}/0"
+                )
+    except:
+        return "Failed to connect to backend"
+    if (response.status_code != 404):
+        return f"Expected 404 status code. Got: {response.status_code}"
+    return ""
+
 if __name__ == '__main__':
-    error = test("Creating a pet", create_test)
-    if error != "":
-        print(error)
-    else:
+    tests = [
+            test("Getting a pet that doesn't exist", test_noget),
+            test("Creating a pet", test_create),
+            test("Getting a pet that does exist", test_get),
+            ]
+    tests_passed = True
+    for test_error in tests:
+        if test_error != "":
+            print(test_error)
+            tests_passed = False
+    if tests_passed: 
         print("All tests passed!")
